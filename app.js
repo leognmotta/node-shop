243bae50fdc4340136290a1494b87a8e1c04fd1a
Leonardo Motta <leo@domboscoatacado.com.br>
2018-12-14 21:49:16 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use('/', (req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
+});
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+});
