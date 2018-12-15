dc05d7c6df1ee88fa445d9e640139573aabc01d0
Leonardo Motta <leo@domboscoatacado.com.br>
2018-12-14 22:25:16 -0200

    Creates public/css/main.css

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
