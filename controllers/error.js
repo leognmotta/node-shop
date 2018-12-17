dd3d210033aaa81c55ac6b7fc5ecbf0ae7639f15
Leonardo Motta <leo@domboscoatacado.com.br>
2018-12-17 20:35:53 -0200

    fixed 404 page

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404Page = (req, res, next) => {
-  res.status(404).render('not_found', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
 };
