7b4a9bd4f0586ca08acdc8a6df900f4348f9466f
Leonardo Motta <leo@domboscoatacado.com.br>
2018-12-17 15:19:31 -0200

    Template engine

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
+  res.status(404).render('not_found', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+app.listen(4000, () => {
+  console.log('http://localhost:8000');
 });
--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product'
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
