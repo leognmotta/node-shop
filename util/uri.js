4db01908cb47b1f5c0124c25bd99fd414d647dc3
Leonardo Motta <leo@domboscoatacado.com.br>
2019-01-04 21:17:17 -0200

    Files upload/download Pagination Async request & Payments

--- util/uri.js
+++ util/uri.js
@@ -1,8 +1,6 @@
 const links = {
-  databaseUri:
-    'mongodb+srv://leomotta121:db686330@cluster0-sud5s.mongodb.net/shop?retryWrites=true',
-  sendgridApiKey:
-    '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'
+  databaseUri: '',
+  sendgridApiKey: ''
 };
 
 module.exports = links;
